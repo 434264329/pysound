5a95f3e1d2ddf383dbe991e9530604e60f3b96c7
434264329 <69955056+434264329@users.noreply.github.com>
2023-07-22 18:31:18 +0800

    Add files via upload

---
+++ sound.py
@@ -0,0 +1,79 @@
+import winsound
+import time
+A = 261  # 1 Do
+B = 293  # 2 Re 
+C = 329  # 3 Mi
+D = 349  # 4 Fa
+E = 392  # 5 So
+F = 440  # 6 La 
+G = 493 # 7  Ti---
+H = 523 # 8 Do   1
+I = 587 #9 Re    2
+J = 659 #10 Mi   3
+K = 698 #11 Fa   4
+L = 783 #12 So   5
+M = 880 #13 La
+N = 987 #14 Ti
+
+duration = 500  # 持续0.5s
+print("一闪 一闪 亮晶晶")
+winsound.Beep(A, duration)
+winsound.Beep(A, duration)
+winsound.Beep(E, duration)
+winsound.Beep(E, duration)
+winsound.Beep(F, duration)
+winsound.Beep(F, duration)
+winsound.Beep(E, duration)
+time.sleep(0.5)
+
+print("满天 都是 小星星")
+winsound.Beep(D, duration)
+winsound.Beep(D, duration)
+winsound.Beep(C, duration)
+winsound.Beep(C, duration)
+winsound.Beep(B, duration)
+winsound.Beep(B, duration)
+winsound.Beep(A, duration)
+time.sleep(0.5)
+
+print("挂在 天上 放光明")
+winsound.Beep(E, duration)
+winsound.Beep(E, duration)
+winsound.Beep(D, duration)
+winsound.Beep(D, duration)
+winsound.Beep(C, duration)
+winsound.Beep(C, duration)
+winsound.Beep(B, duration)
+time.sleep(0.5)
+
+print("他是 我们的 小眼睛")
+winsound.Beep(E, duration)
+winsound.Beep(E, duration)
+winsound.Beep(D, duration)
+winsound.Beep(D, duration)
+winsound.Beep(C, duration)
+winsound.Beep(C, duration)
+winsound.Beep(B, duration)
+time.sleep(0.5)
+
+print("一闪 一闪 亮晶晶")
+winsound.Beep(A, duration)
+winsound.Beep(A, duration)
+winsound.Beep(E, duration)
+winsound.Beep(E, duration)
+winsound.Beep(F, duration)
+winsound.Beep(F, duration)
+winsound.Beep(E, duration)
+time.sleep(0.5)
+
+print("满天 都是 小星星")
+winsound.Beep(D, duration)
+winsound.Beep(D, duration)
+winsound.Beep(C, duration)
+winsound.Beep(C, duration)
+winsound.Beep(B, duration)
+winsound.Beep(B, duration)
+winsound.Beep(A, duration)
+
+
+
---
+++ 国际歌.py
@@ -0,0 +1,242 @@
+import winsound
+from time import sleep
+No = 37
+A = 261  # 1 Do
+B = 293  # 2 Re 
+C = 329  # 3 Mi
+D = 349  # 4 Fa
+E = 392  # 5 So
+F = 440  # 6 La 
+G = 493 # 7  Ti
+H = 523 # 8 Do
+I = 587 #9 Re
+J = 659 #10 Mi
+K = 698 #11 Fa
+L = 783 #12 So
+M = 880 #13 La
+N = 987 #14 Ti
+def The_Internationale_1():
+    winsound.Beep(E,650)
+    winsound.Beep(H,500)
+    sleep(0.2)
+    winsound.Beep(G,300)
+    winsound.Beep(I,350)
+    winsound.Beep(H,90)
+    winsound.Beep(E,85)
+    winsound.Beep(C,300)
+    winsound.Beep(F,500)
+    winsound.Beep(F,600)
+    winsound.Beep(D,750)
+    sleep(0.15)
+    #-------------------------
+    winsound.Beep(F,600)
+    winsound.Beep(I,400)
+    sleep(0.15)
+    winsound.Beep(H,500)
+    winsound.Beep(G,200)
+    winsound.Beep(F,200)
+    winsound.Beep(E,200)
+    winsound.Beep(D,200)
+    winsound.Beep(C,600)
+    sleep(1.25)
+    #-------------------------
+    winsound.Beep(E,710)
+    winsound.Beep(H,400)
+    sleep(0.2)
+    winsound.Beep(G,500)
+    winsound.Beep(I,150)
+    winsound.Beep(H,150)
+    winsound.Beep(E,200)
+    winsound.Beep(C,200)
+    winsound.Beep(F,800)
+    sleep(0.3)
+    winsound.Beep(D,600)
+    sleep(0.2)
+    #-------------------------
+    winsound.Beep(F,700)
+    winsound.Beep(I,500)
+    sleep(0.2)
+    winsound.Beep(H,400)
+    sleep(0.1)
+    winsound.Beep(G,500)
+    winsound.Beep(I,300)
+    winsound.Beep(K,600)
+    winsound.Beep(G,650)
+    winsound.Beep(H,700)
+    winsound.Beep(H,50)
+    sleep(0.6)
+    #-------------------------
+    winsound.Beep(J,600)
+    winsound.Beep(I,350)
+    winsound.Beep(G,350)
+    sleep(0.2)
+    winsound.Beep(F,300)
+    winsound.Beep(G,250)
+    winsound.Beep(H,250)
+    winsound.Beep(F,250)
+    winsound.Beep(G,400)
+    sleep(0.4)
+    #------------------------
+    winsound.Beep(E,400)
+    winsound.Beep(D,400)
+    winsound.Beep(E,600)
+    winsound.Beep(F,600)
+    winsound.Beep(F,600)
+    sleep(0.03)
+    winsound.Beep(I,700)
+    winsound.Beep(H,300) 
+    winsound.Beep(G,600)
+    sleep(0.03)
+    winsound.Beep(G,200) 
+    sleep(0.6)
+    #------------------------
+    winsound.Beep(I,750)
+    winsound.Beep(I,500)
+    winsound.Beep(G,300)
+    winsound.Beep(E,200)
+    winsound.Beep(E,300)
+    winsound.Beep(D,400)
+    winsound.Beep(E,500)
+    winsound.Beep(J,650)
+    sleep(0.03)
+    winsound.Beep(H,750)
+    #----------------------        
+    winsound.Beep(F,400)
+    winsound.Beep(G,400)
+    winsound.Beep(H,100)
+    winsound.Beep(G,200)
+    winsound.Beep(I,530)
+    winsound.Beep(H,530)
+    winsound.Beep(F,500)
+    winsound.Beep(E,700)
+
+    
+def The_Internationale_2():
+    winsound.Beep(E,200)
+    sleep(0.05)
+    winsound.Beep(J,400)#这
+    winsound.Beep(I,450)#是1.
+    winsound.Beep(H,700)#最
+    sleep(0.15)
+    winsound.Beep(E,600)#后
+    winsound.Beep(C,200)#的
+    winsound.Beep(F,700)#斗
+    sleep(0.15)
+    winsound.Beep(D,600)#争
+    sleep(0.05)
+    winsound.Beep(I,400)#团
+    winsound.Beep(H,350)#结
+    winsound.Beep(G,800)#起来
+    sleep(0.05)
+    winsound.Beep(F,400)#到
+    winsound.Beep(E,550)#明
+    winsound.Beep(E,700)#天
+    sleep(0.4)
+    winsound.Beep(E,100)#天
+    sleep(0.5)
+    winsound.Beep(E,600)#英
+    winsound.Beep(J,700)#特
+    sleep(0.02)
+    winsound.Beep(I,550)#纳
+    winsound.Beep(E,550)#雄
+    winsound.Beep(H,550)#耐
+    sleep(0.2)
+    winsound.Beep(G,600)#尔
+    winsound.Beep(G,200)#就
+    winsound.Beep(F,600)#一
+    winsound.Beep(E,300)#定
+    winsound.Beep(F,400)#能
+    winsound.Beep(I,700)#实
+    winsound.Beep(I,700)#现
+    sleep(0.2)
+    winsound.Beep(I,100)
+    
+    winsound.Beep(J,700)#这是最后的斗争
+    winsound.Beep(I,400)
+    winsound.Beep(H,800)
+    sleep(0.2)
+    winsound.Beep(E,700)
+    winsound.Beep(C,200)
+    winsound.Beep(F,700)
+    winsound.Beep(D,650)
+    sleep(0.4)
+    winsound.Beep(I,200)#
+    winsound.Beep(H,200)
+    winsound.Beep(G,700)#起来
+    sleep(0.2)
+    winsound.Beep(F,600)
+    winsound.Beep(E,500)
+    winsound.Beep(J,700)
+    sleep(0.3)
+    winsound.Beep(J,700)#英特纳雄耐尔就一定要实现
+    winsound.Beep(L,400)#5.
+    sleep(0.2)
+    winsound.Beep(K,400)#4.
+    winsound.Beep(J,400)#3.
+    winsound.Beep(I,700)#2.
+    winsound.Beep(J,300)#3.
+    winsound.Beep(K,700)#4. 尔
+    sleep(0.2)
+    winsound.Beep(K,200)#4.
+    winsound.Beep(J,300)#3.
+    winsound.Beep(J,300)
+    winsound.Beep(I,500)
+    winsound.Beep(I,500)
+    winsound.Beep(H,700)
+    sleep(2)
+print("起来,饥寒交迫的奴隶")
+print("起来,全世界受苦的人")
+print("满腔的热血已经沸腾")
+print("要为真理而斗争")
+print("旧世界打个落花流水")
+print("奴隶们起来 起来")
+print("不要说我们一无所有")
+print("我们要做天下的主人")
+The_Internationale_1()
+print("这是最后的斗争")
+print("团结起来到明天")
+print("英特纳雄耐尔就一定要实现")
+print("这是最后的斗争")
+print("团结起来到明天")
+print("英特雄纳耐尔就一定要实现")
+The_Internationale_2()
+##段落一
+print("从来就没有什么救世主")
+print("也不靠神仙皇帝")
+print("要创造人类的幸福")
+print("全靠我们自己")
+print("我们要夺回劳动果实")
+print("让思想冲破牢笼")
+print("快把那炉火少的通红")
+print("趁热打铁才能成功")
+The_Internationale_1()
+print("这是最后的斗争")
+print("团结起来到明天")
+print("英特纳雄耐尔一定要实现")
+print("这是最后的斗争")
+print("团结起来到明天")
+print("英特纳雄耐尔就一定要实现")
+The_Internationale_2()
+#段落2
+print("是谁创造了人类世界")
+print("是我们劳动群众")
+print("一切归劳动者所有哪能容得寄生虫")
+print("最可恨那些毒蛇猛兽")
+print("吃尽了我们的血肉")
+print("一旦把他们消灭干净")
+print("鲜红的太阳找遍全球")
+The_Internationale_1()
+print("这是最后的斗争")
+print("团结起来到明天")
+print("英特纳雄耐尔就一定要实现")
+print("这是最后的斗争")
+print("团结起来到明天")
+print("英特那雄耐尔就一定要实现")
+The_Internationale_2()
+print("五秒后退出程序")
+sleep(5)
+#段落3
+
+
+
+
